83a20d6573433b20c46bdf18e4b55a1c54755d64
Vladimir Vorobyev <vorobvla@fit.cvut.cz>
2015-06-09 17:19:49 +0200

    Attempt to implment argument parsing with argparse subparsers

--- brainx/__main__.py
+++ brainx/__main__.py
@@ -4,38 +4,39 @@
 from argparse import ArgumentParser
 from argparse import ArgumentTypeError
 from argparse import Action
+from sys import argv
 
-# store one or two arguments (add a None if second is missing)
-def store_one_or_two():
-    class OneOrTwo(Action):
-        def __call__(self, parser, namespace, values, option_string=None):
-            print('DB')
-            if 1 <= len(values) <= 2:
-                if len(values) == 1:
-                    values.append(None)
-                pass
-            else:
-                raise ArgumentTypeError('One or two arguments expected with option {}.'.format(self.dest))
-            setattr(namespace, self.dest, values)
-    return OneOrTwo
 
 
 parser = ArgumentParser(description='TODO', epilog='TODO')
 
 # --- adding parameters according to functional requirements ---
 
-#   a) Interpreter itself
-parser.add_argument('sourcefile', nargs='?', help='Name of file with sourcecode to be executed'
-                                                  '(in brainfuck, brainlotter or '
-                                                  'braincopter). If missing, the interpreter switches'
-                                                  ' to interactive mode.')
-#   b) Translation of picture to stdout or text file
+
+if '--lc2f' or '--f2lc' or '-h' or '--help' in argv:
+    # Translater LC2F
+    # b) Translation of picture to stdout or text file
+    subparsers = parser.add_subparsers()
+    subparser_lc2f = subparsers.add_parser('--lc2f', help='Translates picture (brainlotter or braincopter) '
+                                                     'to brinfuck sourcecode.')
+    subparser_f2lc = subparsers.add_parser('--f2lc', help='')
+
+    subparser_lc2f.add_argument('input_file', nargs=1, help='Name of source file to be translated.')
+    subparser_lc2f.add_argument('output_file', nargs='?', help='Name of target file to contain ctanslated code. '
+                                                               'If missing, code is sent to stdout.')
+    subparser_f2lc.add_argument('-i', nargs=2, help='input file')
+    subparser_f2lc.add_argument('-o', nargs=1, help='output file')
+else:
+    # No translater
+    # a) Interpreter itself
+    parser.add_argument('sourcefile', nargs='?', help='Name of file with sourcecode to be executed'
+                                                      '(in brainfuck, brainlotter or '
+                                                      'braincopter). If missing, the interpreter switches'
+                                                      ' to interactive mode.')
 # TODO: remove '...' in methavar (occurs beacuse of nargs='+').
-parser.add_argument('--lc2f', nargs='+', action=store_one_or_two(), help='Translates picture (brainlotter or braincopter)'
-                    'to brinfuck sourcecode. If name of output file missing sends output to stdout.',
-                    metavar=('input_file', 'output_file'))
 #   c) Translation of text file (brainfuck sourcecode) to brainlotter or braincopter image
-parser.add_argument()
+
+
 #   d) Test output to file
 #   e) Advanced testing options
 #   f) Help (implemented by angpars as default)
