ca85bd7bdbd8ad86e863b8c7661e4074479dde4c
Vladimir Vorobyev <vorobvla@fit.cvut.cz>
2015-06-12 18:56:28 +0200

    A bit improved png analyzing

--- brainx/graphic_langs.py
+++ brainx/graphic_langs.py
@@ -22,15 +22,20 @@ def analyze_png(filename):
             img_heigth = None
             while True:
                 data_len = btoi(file.read(4))
-                head = file.read(4)
-                if head == b'IHDR':
+                chunk = file.read(4)
+                if chunk == b'IHDR':
                     # analyze IHDR
                     img_width = btoi(file.read(4))
                     img_heigth = btoi(file.read(4))
-                    if file.read(5) != b'\x08\x02\x00\x00\x00':
+                    other_opts = file.read(5)
+                    if other_opts != b'\x08\x02\x00\x00\x00' and other_opts != b'\x08\x02\x00\x00\x01':
                         raise PNGNotImplementedError
                     else:
                         break
+                elif chunk == b'IEND':
+                    break
+                else:
+                    file.read(data_len)
     print(data_len)
     print(img_width)
     print(img_heigth)
