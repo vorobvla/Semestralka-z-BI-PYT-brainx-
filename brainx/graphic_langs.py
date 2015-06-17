fa3a5a5ef4bc9deae2dd0985ea9e95d57342adff
Vladimir Vorobyev <vorobvla@fit.cvut.cz>
2015-06-17 18:42:43 +0200

    Parcing of decompressed data to rows works properly

--- brainx/graphic_langs.py
+++ brainx/graphic_langs.py
@@ -33,7 +33,7 @@ def process_png(filename):
             # analyze file. finish when IEND chunk found
             img_width = None
             img_heigth = None
-            comprimated_img_data = bytes()
+            compressed_img_data = bytes()
             while True:
                 read = file.read(4)
                 print('READ: ' + str(read))
@@ -55,12 +55,12 @@ def process_png(filename):
                     img_heigth = btoi(data[8:12])
                     other_opts = data[12:17]
 
-                    if other_opts != b'\x08\x02\x00\x00\x00' and other_opts != b'\x08\x02\x00\x00\x01':
+                    if other_opts != b'\x08\x02\x00\x00\x00':
                         raise PNGNotImplementedError
                     continue
                 elif chunk == b'IDAT':
                     # append data
-                    comprimated_img_data += data[4:]
+                    compressed_img_data += data[4:]
                 elif chunk == b'IEND':
                     print('REACHED END')
                     break
@@ -70,8 +70,21 @@ def process_png(filename):
                     pass
                     # skip chunks that are not interesting for us
                     #file.read(data_len + 4)
-        print(comprimated_img_data)
-        return decompress(comprimated_img_data), img_width, img_heigth
+        #print(compressed_img_data)
+        # decompress and defilter image data
+        decompressed_img_data = decompress(compressed_img_data)
+        row_idx = 0
+        img_data = bytes()
+        print('DECOMPRESSED DATA, {}x{}\n-------\n'.format(img_width,img_heigth))
+        #print(decompressed_img_data)
+        decompressed_data_w = img_width * 3 + 1
+        while row_idx < img_heigth:
+            row = decompressed_img_data[row_idx * decompressed_data_w: (row_idx + 1) * decompressed_data_w]
+            print(row)
+            #img_data += decompressed_img_data[1:]
+            row_idx += 1
+        print('-------')
+        return decompress(img_data, img_width, img_heigth)
 
 
 # parces colorcode from binary data
