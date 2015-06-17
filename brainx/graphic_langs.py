e89e89aefcf88cf6e5ad9b8328d16740342564dc
Vladimir Vorobyev <vorobvla@fit.cvut.cz>
2015-06-17 22:41:23 +0200

    Defiltering of image data implemented

--- brainx/graphic_langs.py
+++ brainx/graphic_langs.py
@@ -2,6 +2,9 @@
 from zlib import decompress
 from zlib import crc32
 
+LANG_BRAINLOLLER = 0
+LANG_BRAINCOPTER = 1
+
 
 class FileErrorException(Exception):
     pass
@@ -17,11 +20,58 @@ class PNGNotImplementedError(Exception):
 btoi = lambda b: int.from_bytes(b, byteorder='big', signed=False)
 
 
+# returns False if name containg anything different from capital latter
 def obligatory_chunk(name):
     for ch in name:
         if not ord('A') <= ch <= ord('Z'):
             return False
     return True
+
+
+# needed for defilter, copypaste from lections
+def paeth(a, b, c):
+    p = a + b - c
+    pa = abs(p - a)
+    pb = abs(p - b)
+    pc = abs(p - c)
+    if pa <= pb and pa <= pc:
+        return a
+    elif pb <= pc:
+        return b
+    else:
+        return c
+
+
+# prev_row does not contain type!
+def defilter(row, prev_row):
+    # retrieve type and remove sype from row
+    filt_type = row[0]
+    row = row[1:]
+    output_row = bytearray()
+    # previous pixels (as defined in w3 specification)
+    a = lambda idx: 0 if idx - 3 < 0 else row[idx - 3]
+    b = lambda idx: 0 if prev_row is None else prev_row[idx]
+    c = lambda idx: 0 if idx - 3 < 0 or prev_row is None else prev_row[idx - 3]
+    #print('Type: {}'.format(filt_type))
+
+    if filt_type == 0:
+        return row
+    elif filt_type == 1:
+        for i, x in enumerate(row):
+            output_row.append((x + a(i)) % 256)
+    elif filt_type == 2:
+        for i, x in enumerate(row):
+            output_row.append((x + b(i)) % 256)
+    elif filt_type == 3:
+        for i, x in enumerate(row):
+            output_row.append((x + (a(i) + b(i)) // 2) % 256)
+    elif filt_type == 4:
+        for i, x in enumerate(row):
+            #print('i = {}, a = {}, b = {}, c = {}'.format(i, a(i), b(i), c(i)))
+            output_row.append((x + paeth(a(i), b(i), c(i))) % 256)
+    else:
+        raise FileErrorException('Illegal filtration type {} occured'.format(filt_type))
+    return bytes(output_row)
 
 
 def process_png(filename):
@@ -47,7 +97,7 @@ def process_png(filename):
                 # read crc
                 crc = btoi(file.read(4))
                 if crc32(data) != crc:
-                    raise FileErrorException('File data currupted (CRC data mismatch)')
+                    raise FileErrorException('File data currupted (CRC mismatch)')
                 chunk = data[:4]
                 if chunk == b'IHDR':
                     # analyze IHDR
@@ -65,26 +115,28 @@ def process_png(filename):
                     print('REACHED END')
                     break
                 elif obligatory_chunk(chunk):
-                    raise FileErrorException('Unexpected obligatory chunk {} in file {}'.format(str(chunk), filename))
+                    raise NotImplementedError('Unexpected obligatory chunk {} in file {}'.format(str(chunk), filename))
                 else:
                     pass
-                    # skip chunks that are not interesting for us
-                    #file.read(data_len + 4)
         #print(compressed_img_data)
         # decompress and defilter image data
         decompressed_img_data = decompress(compressed_img_data)
         row_idx = 0
-        img_data = bytes()
-        print('DECOMPRESSED DATA, {}x{}\n-------\n'.format(img_width,img_heigth))
+        img_data = []
+        print('DECOMPRESSED DATA, {}x{}\n-------\n'.format(img_width, img_heigth))
         #print(decompressed_img_data)
         decompressed_data_w = img_width * 3 + 1
+        previous_row = None
         while row_idx < img_heigth:
             row = decompressed_img_data[row_idx * decompressed_data_w: (row_idx + 1) * decompressed_data_w]
-            print(row)
-            #img_data += decompressed_img_data[1:]
+            print('Processing : {}'.format(row))
+            row = (defilter(row, previous_row))
+            print('Processed : {}\n'.format(row))
+            previous_row = row
             row_idx += 1
+        #print(img_data)
         print('-------')
-        return decompress(img_data, img_width, img_heigth)
+        return img_data, img_width, img_heigth
 
 
 # parces colorcode from binary data
