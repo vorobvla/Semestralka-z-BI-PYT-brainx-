2b8ed90dd1164f263f5e19570c1b7dc14d2ebcf8
Vladimir Vorobyev <vorobvla@fit.cvut.cz>
2015-06-19 01:14:28 +0200

    Forgot to add mainainance/__init__.py and mantainance/logger.py to previous commit. This commit adds these files, but the message from the previous commit is valid for this one

---
+++ brainx/maintainance/logger.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+
+class Logger():
+    file_num = 1
+
+    # all args must be strings (for conveniance)
+    @staticmethod
+    def log_to_file(program_data=None, memory=None, memory_pointer=None, output=None, rgb_input=None,
+                    rgb_input_not_bl=None, rgb_output=None):
+        # UNIX endline
+        lf = chr(10)
+        with open('debug_{:02}.log'.format(Logger.file_num), encoding='ASCII', mode='w') as debug_file:
+            if program_data is not None:
+                debug_file.write('# program data' + lf + program_data + lf + lf)
+            if memory is not None:
+                debug_file.write('# memory' + lf + memory + lf + lf)
+            if memory is not None:
+                debug_file.write('# memory pointer' + lf + memory_pointer + lf + lf)
+            if memory is not None:
+                debug_file.write('# output' + lf + output + lf + lf)
+            if rgb_input is not None:
+                debug_file.write('# RGB input' + lf + rgb_input)
+                if rgb_input_not_bl is not None:
+                    debug_file.write(lf + rgb_input_not_bl + lf + lf)
+                else:
+                    debug_file.write(lf + lf)
+            if rgb_output is not None:
+                debug_file.write('# RGB output' + lf + rgb_output + lf + lf)
+
+        Logger.file_num = Logger.file_num + 1 if Logger.file_num < 99 else 1
+
